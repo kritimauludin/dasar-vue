654bfce02da0a20ef8643da96344eb4f2881f8e6
Kriti Mauludin <kritimauludin24@gmail.com>
2022-08-03 19:48:28 +0700

    modul 3 : Introduction

---
+++ app.js
@@ -0,0 +1,35 @@
+new Vue({
+    el: '#app', 
+    data: {
+        title: 'Learn vue js',
+        codeName : 'RZL',
+        name: 'Rizil',
+        coordinate: {
+            x: 0,
+            y: 0
+        },
+        url: 'https://arahin.ocumps.com',
+        text: ['one', 'two'],
+        courses: ['Coding JS', 'Cooking', 'Promotion', 'SEO'],
+        packages: [
+            {coursePackage:'Coding JS', price: 500000, description: 'Coding with javascript programming language'},
+            {coursePackage:'Cooking', price: 200000, description: 'Learn how to cooking'},
+            {coursePackage:'SEO', price: 250000, description: 'Learn how to implementation SEO in your website'}
+        ]
+    },
+    methods: {
+        greet(){
+            return `Hello world... | My Code ${this.codeName}`
+        },
+        logEvent(e){
+            console.log(e)
+        },
+        logCoordinate(e){
+            this.coordinate.x = e.offsetX
+            this.coordinate.y = e.offsetY
+        },
+        updateName (e){
+            this.name = e.target.value
+        }
+    }
+})
